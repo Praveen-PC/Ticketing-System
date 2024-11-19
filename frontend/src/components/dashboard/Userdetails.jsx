147b0c38ecf8a4ce93594266e858cb1a32d5dcad
Praveen Chakravarthy <praveenchakravarthy28@gmail.com>
2024-11-19 16:42:22 +0530

    Ticketing Sysytem

--- frontend/src/components/dashboard/Userdetails.jsx
+++ frontend/src/components/dashboard/Userdetails.jsx
@@ -3,12 +3,11 @@ import Header from "./Header";
 import axios from "axios";
 import DataTable from "react-data-table-component";
 
-
 const Userdetails = () => {
   const [data, setdata] = useState([]);
   const [searchTerm, setSearhTerm] = useState("");
   const [filterData, setFilterData] = useState([]);
-  const [role,setrole]=useState("")
+  const [role, setrole] = useState("");
 
   const fetchUserDta = async () => {
     try {
@@ -28,63 +27,103 @@ const Userdetails = () => {
   const handleSearch = (e) => {
     const value = e.target.value;
     setSearhTerm(value);
-    const filter = data.filter((item) =>
-      item.name.toLowerCase().includes(value.toLowerCase())||
-      item.phoneNo.toLowerCase().includes(value.toLowerCase())||
-      item.role.toLowerCase().includes(value.toLowerCase())
+    const filter = data.filter(
+      (item) =>
+        item.name.toLowerCase().includes(value.toLowerCase()) ||
+        item.phoneNo.toLowerCase().includes(value.toLowerCase()) ||
+        item.role.toLowerCase().includes(value.toLowerCase())
     );
     setFilterData(filter);
   };
 
-  const handleDelete=async(id)=>{
-    try{
-        const response=await axios.delete(`http://localhost:8080/api/deleteuser/${id}`)
-        console.log(response.data,"is deleted")
-        fetchUserDta()
-    }catch(error){
-        console.log(error)
+  const handleDelete = async (id) => {
+    try {
+      const response = await axios.delete(
+        `http://localhost:8080/api/deleteuser/${id}`
+      );
+      console.log(response.data, "is deleted");
+      fetchUserDta();
+    } catch (error) {
+      console.log(error);
     }
-  }
- const handleEdit=async(id,currentRole)=>{
-    const newRole=currentRole==='admin'?'user':'admin'
-    setrole(role)
-    try{
-        const response=await axios.put(`http://localhost:8080/api/updateuser/${id}`,{role:newRole})
-        console.log(response.data)
-        fetchUserDta()
-    }catch(error){
-        console.log(error)
+  };
+  const handleEdit = async (id, currentRole) => {
+    const newRole = currentRole === "admin" ? "user" : "admin";
+    setrole(role);
+    try {
+      const response = await axios.put(
+        `http://localhost:8080/api/updateuser/${id}`,
+        { role: newRole }
+      );
+      console.log(response.data);
+      fetchUserDta();
+    } catch (error) {
+      console.log(error);
     }
- }
+  };
 
-const columns = [
-    // { name: "ID", sortable: true, selector: (row) => row.id, className: 'column-id' },
-    { name: "Name", selector: (row) => row.name, className: 'column-name' },
-    { name: "Number", selector: (row) => row.phoneNo, style: { minWidth: '150px', textAlign: 'center' }, className: 'column-phoneNo' },
-    { name: "Role", selector: (row) => row.role, sortable: true, className: 'column-role' },
-    { name: "Change_Role", 
-      cell: (row) => (
-        <div className="">
-          {row.role === 'admin' ?  
-           <button className="btn btn-outline-success btn-sm" onClick={() => handleEdit(row.id,row.role)}>User</button> 
-            : <button className="btn btn-outline-primary btn-sm"  onClick={() => handleEdit(row.id,row.role)}>Admin</button>
-          }</div>)
+  const columns = [
+    {
+      name: <h6 className="fw-bold  ">Name</h6>,
+      selector: (row) => row.name,
+      className: "column-name",
     },
-    {   name:'Action',
-        cell:row=>(
-            <button className="btn btn-outline-danger btn-sm " onClick={() => handleDelete(row.id)}>
-            <i className="fa-solid fa-trash"></i></button>)
-    }
+    {
+      name: <h6 className="fw-bold">Number</h6>,
+      selector: (row) => row.phoneNo,
+      style: { minWidth: "100px", textAlign: "center" },
+    },
+    {
+      name: <h6 className="fw-bold ">Role</h6>,
+      selector: (row) => (
+        <p className={`fw-bold ${row.role === "admin" ? "text-primary" : ""}`}>
+          {row.role}
+        </p>
+      ),
+      sortable: true,
+    },
+
+    {
+      name: <h6 className="fw-bold  ">ChangeRole</h6>,
+      cell: (row) => (
+        <>
+          {row.role === "admin" ? (
+            <button
+              className="btn btn-outline-secondary btn-sm"
+              onClick={() => handleEdit(row.id, row.role)}
+            >
+              User
+            </button>
+          ) : (
+            <button
+              className="btn btn-outline-secondary btn-sm"
+              onClick={() => handleEdit(row.id, row.role)}
+            >
+              Admin
+            </button>
+          )}
+        </>
+      ),
+    },
+    {
+      name: <h6 className="fw-bold">Action</h6>,
+      cell: (row) => (
+        <button
+          className="btn btn-outline-danger btn-sm "
+          onClick={() => handleDelete(row.id)}
+        >
+          <i className="fa-solid fa-trash"></i>
+        </button>
+      ),
+    },
   ];
-  
-  
 
   return (
     <>
       <Header />
       <div className="container mt-3">
         <div className="d-flex justify-content-between gap-4">
-          <h3 className="fw-bold">User_Details</h3>
+          <h4 className="fw-bold">User_Details</h4>
           <input
             type="search"
             className="form-control"
@@ -103,7 +142,6 @@ const columns = [
               highlightOnHover
               pointerOnHover
               noHeader
-              
             ></DataTable>
           </div>
         </div>
