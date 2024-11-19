e64a73662a782f91cda0191d3cbd31d70653430a
Praveen Chakravarthy <praveenchakravarthy28@gmail.com>
2024-11-19 17:56:55 +0530

    logout

--- frontend/src/components/dashboard/Header.jsx
+++ frontend/src/components/dashboard/Header.jsx
@@ -5,6 +5,7 @@ import { Link, useNavigate } from "react-router-dom";
 
 const Header = ({role}) => {
     const navigate=useNavigate()
+    const token=sessionStorage.getItem('authtoken')
     const handleLogout=()=>{
         sessionStorage.removeItem('authtoken')
         navigate('/')        
@@ -19,9 +20,10 @@ const Header = ({role}) => {
         <div class="container-fluid">
         <Link to="/dashboard" className="navbar-brand mb-0 h1 text-decoration-none fw-bold">Navbar</Link>
         <div className="d-flex justify-content-around">
-          {userRole==='admin'?<Link to="/userdetails" className="btn  btn-outline-none border-0" ><i class="fa-solid fa-users"></i></Link>:" "}  
+          {userRole==='admin'?<Link to="/userdetails" className="btn  btn-outline-none border-0" ><i class="fa-solid fa-users"></i></Link>:" "} 
+
+          {!token ?" ":<button className="btn  border-0" onClick={handleLogout}><i class="fa-solid fa-right-from-bracket"></i></button>} 
         
-        <button className="btn  border-0" onClick={handleLogout}><i class="fa-solid fa-right-from-bracket"></i></button>
         </div>
         
         </div>
